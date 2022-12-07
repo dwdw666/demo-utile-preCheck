6265d89f2330c1348dbaa746d282ae560e8a4d4e
wangtaojun <wangtaojun01@corp.netease.com>
2022-12-07 15:24:27 +0800

    new files

---
+++ src/main/java/com/example/demoutilepreCheck/DemoUtilePreCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoutilepreCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUtilePreCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoUtilePreCheckApplication.class, args);
+	}
+
+}
